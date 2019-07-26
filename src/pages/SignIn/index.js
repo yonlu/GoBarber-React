e42f994cab2457435716f61f97690d13d1b6b312
yonlu <lss.emailbox@gmail.com>
2019-07-26 16:18:59 -0400

    chore: intitial commit

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import { signInRequest } from '~/store/modules/auth/actions';
+
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Enter a valid email')
+    .required('An email is required'),
+  password: Yup.string().required('A password is required'),
+});
+
+export default function SignIn() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber logo" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Your email" />
+        <Input name="password" type="password" placeholder="Your password" />
+
+        <button type="submit">Log in</button>
+        <Link to="/register">Create an account</Link>
+      </Form>
+    </>
+  );
+}
---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('A name is required'),
+  email: Yup.string()
+    .email('Enter a valid email')
+    .required('An email is required'),
+  password: Yup.string()
+    .min(6, 'Password should have at least 6 characters')
+    .required('A password is required'),
+});
+
+export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber logo" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Your name" />
+        <Input name="email" type="email" placeholder="Your email" />
+        <Input name="password" type="password" placeholder="Your password" />
+
+        <button type="submit">Sign up</button>
+        <Link to="/">Already have an account? Sign in</Link>
+      </Form>
+    </>
+  );
+}
