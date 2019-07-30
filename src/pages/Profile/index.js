158a8f72771b7ac8ee9b025b7e7b80e35083a44b
yonlu <lss.emailbox@gmail.com>
2019-07-30 14:39:01 -0400

    feature: users are able to update their profiles

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,55 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+
+import { signOut } from '~/store/modules/auth/actions';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import AvatarInput from './AvatarInput';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const dispatch = useDispatch();
+
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+
+        <Input name="name" placeholder="Full name" />
+        <Input name="email" type="email" placeholder="Your email" />
+
+        <hr />
+
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Current password"
+        />
+        <Input name="password" type="password" placeholder="New password" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirm new password"
+        />
+
+        <button type="submit">Update Profile</button>
+      </Form>
+
+      <button type="button" onClick={handleSignOut}>
+        Log out
+      </button>
+    </Container>
+  );
 }
--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,3 +1,30 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import api from '~/services/api';
+
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, avatar_id, ...rest } = payload.data;
+
+    const profile = Object.assign(
+      { name, email, avatar_id },
+      rest.oldPassword ? rest : {}
+    );
+
+    const response = yield call(api.put, 'users', profile);
+
+    toast.success('Profile updated successfully');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (err) {
+    toast.error(
+      'Failed to update profile, recheck the information you entered.'
+    );
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
